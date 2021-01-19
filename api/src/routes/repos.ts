29a06a048f5c32b2c7c8fc80e83cd2ca72bf0ed3
My Name <me@example.com>
2021-01-19 08:06:42 -0500

    Read file from data folder and filtered those repos for boolean fork and sent repo as response

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,27 @@
 import { Router, Request, Response } from 'express';
+import { readFile } from 'fs';
 
 export const repos = Router();
 
+let allRepos = [];
+
+repos.use(function(req,res,next){
+  readFile('./data/repos.json', (err, data) => {
+    if (!err) {
+      allRepos = JSON.parse(data);
+    }
+  })
+  next();
+})
+
 repos.get('/', async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
+  res.header({'Cache-Control':'no-store','Content-Type':'application/json'})
+  if (allRepos.length > 0) {
+    allRepos = allRepos.filter(repo => repo.fork===false);
+  }
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(allRepos);
 });

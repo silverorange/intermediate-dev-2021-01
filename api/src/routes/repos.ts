17977c983b2ae3d6846628eeda3a3894e7c0ac3b
My Name <me@example.com>
2021-01-19 08:55:55 -0500

    installed node fetch and fetched repos and added them to be sent as response

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,27 +1,44 @@
-import { Router, Request, Response } from 'express';
-import { readFile } from 'fs';
+import { Router, Request, Response } from 'express'
+import { readFile } from 'fs'
+import fetch from 'node-fetch'
 
-export const repos = Router();
+export const repos = Router()
 
-let allRepos = [];
+let allRepos = []
 
-repos.use(function(req,res,next){
+repos.use(function (req, res, next) {
   readFile('./data/repos.json', (err, data) => {
     if (!err) {
-      allRepos = JSON.parse(data);
+      allRepos = JSON.parse(data)
     }
   })
-  next();
+  next()
 })
 
-repos.get('/', async (_: Request, res: Response) => {
-  res.header({'Cache-Control':'no-store','Content-Type':'application/json'})
+repos.use(async function (req, res, next) {
+  try {
+    let response = await fetch(
+      'https://api.github.com/users/silverorange/repos'
+    )
+    let fetchedRepos = await response.json()
+    allRepos = [...allRepos, ...fetchedRepos]
+  } catch (err) {
+    //
+  }
+  next()
+})
+
+repos.get('/', (_: Request, res: Response) => {
+  res.header({
+    'Cache-Control': 'no-store',
+    'Content-Type': 'application/json'
+  })
   if (allRepos.length > 0) {
-    allRepos = allRepos.filter(repo => repo.fork===false);
+    allRepos = allRepos.filter(repo => repo.fork === false)
   }
 
-  res.status(200);
+  res.status(200)
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json(allRepos);
-});
+  res.json(allRepos)
+})
